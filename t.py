50c0c310848e667866206e1930fa134e87294ee7
Jesus390 <jesusespinola90@hotmail.com>
2022-12-25 15:15:36 -0300

    template for python

--- t.py
+++ t.py
@@ -1,9 +1,1 @@
-from skimage import data, io, filters
-
-image = data.coins()
-print(f"{image=}")
-print(f"{len(image)=}")
-# ... or any other NumPy array!
-edges = filters.sobel(image)
-io.imshow(edges)
-io.show()
+print("Hello World")
